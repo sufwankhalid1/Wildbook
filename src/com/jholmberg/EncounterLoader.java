3a7479573570436a45ee9481b45f6375b653cdb9
holmbergius <holmbergius@gmail.com>
2011-01-16 10:26:00 -0800

    Fixed an improper import

--- src/com/jholmberg/EncounterLoader.java
+++ src/com/jholmberg/EncounterLoader.java
@@ -1,6 +1,6 @@
 package com.jholmberg;
 /**
- * 
+ *
  */
 
 
@@ -18,7 +18,7 @@ import org.joda.time.DateTime;
 import org.joda.time.format.*;
 
 //import jackcess
-import com.healthmarketscience.*;
+//import com.healthmarketscience.*;
 import com.healthmarketscience.jackcess.*;
 import com.healthmarketscience.jackcess.query.*;
 import com.healthmarketscience.jackcess.scsu.*;
@@ -34,48 +34,48 @@ public class EncounterLoader {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
+
+
 		String urlToThumbnailJSPPage="http://www.splashcatalog.org/shepherd-alpha1/resetThumbnail2.jsp";
-		
+
 		System.out.println("\n\nStarting thumbnail work!");
-		
+
 		int numThumbnailsToGenerate=36578;
 		String IDKey="";
 		for(int q=32166;q<numThumbnailsToGenerate;q++){
 			System.out.println(q);
 			//ping a URL to thumbnail generator - Tomcat must be up and running
-		    try 
+		    try
 		    {
-		        
+
 		    	//System.out.println("Trying to render a thumbnail for: "+IDKey+ "as "+thumbnailTheseImages.get(q));
 		    	String urlString=urlToThumbnailJSPPage+"?number="+q;
 		    	//System.out.println("     "+urlString);
 		    	URL url = new URL(urlString);
-		    
+
 		        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 		        in.close();
-		    } 
+		    }
 		    catch (MalformedURLException e) {
-		    	
+
 		    	System.out.println("Error trying to render the thumbnail for "+IDKey+".");
 		    	e.printStackTrace();
-		    	
+
 		    }
 		    catch (IOException ioe) {
-		    	
+
 		    	System.out.println("Error trying to render the thumbnail for "+IDKey+".");
 		    	ioe.printStackTrace();
-		    	
-		    } 
-		    
-			
-			
+
+		    }
+
+
+
 		}
-		
+
 
 	}
-	
-	
+
+
 
 }
--- src/com/jholmberg/FieldFixer.java
+++ src/com/jholmberg/FieldFixer.java
@@ -1,6 +1,6 @@
 package com.jholmberg;
 /**
- * 
+ *
  */
 
 
@@ -18,7 +18,7 @@ import org.joda.time.DateTime;
 import org.joda.time.format.*;
 
 //import jackcess
-import com.healthmarketscience.*;
+//import com.healthmarketscience.*;
 import com.healthmarketscience.jackcess.*;
 import com.healthmarketscience.jackcess.query.*;
 import com.healthmarketscience.jackcess.scsu.*;
@@ -36,46 +36,46 @@ public class FieldFixer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
+
+
 		//initial environment config
 		String pathToAccessFile="C:\\splash\\SPLASH All Seasons.mdb";
-		
+
 		String pathToUpdateFile="C:\\splash\\CRC SPLASHID additional sightings.mdb";
-		
+
 		String encountersDirPath="C:\\tomcat6\\webapps\\shepherd-alpha2\\encounters";
 		String splashImagesDirPath="C:\\splash\\SPLASH Images";
 		String urlToThumbnailJSPPage="http://localhost:8080/shepherd-alpha1/resetThumbnail.jsp";
-		
+
 		//an arraylist for later thumbnail generation
 		ArrayList<String> thumbnailThese=new ArrayList<String>();
 		ArrayList<String> thumbnailTheseImages=new ArrayList<String>();
-		
+
 		//let's get our Shepherd Project structures built
 		Shepherd myShepherd = new Shepherd();
-		
+
 		//let's load our Access database
 		File accessDB=new File(pathToAccessFile);
 		File updateDB=new File(pathToUpdateFile);
-		
+
 		try{
-			
+
 			//lets' get to work!!!!!!!
 			Database db=Database.open(accessDB);
 			Database uDB=Database.open(updateDB);
 			File copyImagesFromDir=new File(splashImagesDirPath);
 			File encountersRootDir=new File(encountersDirPath);
-			
+
 			//update changes
 			//Table tDailyEffort=db.getTable("tDailyEffort");
 			//Table tSightings=db.getTable("tSightings");
 			//Table tIdentifications=db.getTable("tIdentifications");
-			
+
 			Table tDailyEffort=uDB.getTable("tDailyEffort");
 			Table tSightings=uDB.getTable("tSightings");
 			Table tIdentifications=uDB.getTable("tIdentifications");
-			
-			
+
+
 			Table tSPLASHIDFilenames=db.getTable("tSPLASHIDFilenames");
 			Table tSPLASHIDSexes=db.getTable("tSPLASHIDSexes");
 			Table tFlukeQualCodes=db.getTable("tFlukeQualCodes");
@@ -83,11 +83,11 @@ public class FieldFixer {
 			Table tRegion=db.getTable("ltRegion");
 			Table ltResearchGroup=db.getTable("ltResearch Group");
 			Table tSampleLabData=db.getTable("tSampleLabData");
-			
-			
+
+
 			//first, let's get the behaviorindex and populate an ArrayList
-			
-			
+
+
 			Iterator<Map<String,Object>> tBehaviorCodesIterator = tBehaviorIndex.iterator();
 			TreeMap<String,String> behMap = new TreeMap<String,String>();
 			while(tBehaviorCodesIterator.hasNext()){
@@ -96,14 +96,14 @@ public class FieldFixer {
 				String name=(String)thisIndexRow.get("Individual Role");
 				if(!behMap.containsKey(index)){
 					behMap.put(index, name);
-					
+
 				}
 			}
-			
-			
-			
+
+
+
 			//first, let's get the region index and populate an ArrayList
-			
+
 			Iterator<Map<String,Object>> tRegionCodesIterator = tRegion.iterator();
 			TreeMap<String,String> regionMap = new TreeMap<String,String>();
 			while(tRegionCodesIterator.hasNext()){
@@ -114,9 +114,9 @@ public class FieldFixer {
 					regionMap.put(index, name);
 					System.out.println("Adding region: "+index+", "+name);
 				}
-				
+
 			}
-			
+
 			//first, let's get the research group index and populate an ArrayList
 			Iterator<Map<String,Object>> tRGIterator = ltResearchGroup.iterator();
 			TreeMap<String,String> rgMap = new TreeMap<String,String>();
@@ -129,8 +129,8 @@ public class FieldFixer {
 					System.out.println("Adding research group: "+index+", "+name);
 				}
 			}
-			
-			
+
+
 			//1. We start with tIdentifications and we only use rows that have a SPlashID. There may be more than one entry.
 			Iterator<Map<String,Object>> tIdentificationsIterator = tIdentifications.iterator();
 			int numMatchingIdentifications=0;
@@ -141,40 +141,40 @@ public class FieldFixer {
 					numMatchingIdentifications++;
 
 					//if(numMatchingIdentifications<10){
-					
+
 					//update changes
 					processThisRow(thisRow, myShepherd, splashImagesDirPath, encountersDirPath, tSPLASHIDFilenames, urlToThumbnailJSPPage, tSPLASHIDSexes, tSightings, thumbnailThese, thumbnailTheseImages, tDailyEffort, tFlukeQualCodes, tBehaviorIndex, behMap, regionMap, rgMap, tSampleLabData);
-					
-					
-					
+
+
+
 					//}
 				}
 
-				
-				
-				
+
+
+
 			}
-			
-			
+
+
 			//2. Then we link over to table tSightings to build encounters for each markedindividual loaded from tIdentifications.
-			
-			
-			
-			
-			  
-			
-			
+
+
+
+
+
+
+
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
 		myShepherd.closeDBTransaction();
-		
 
-		
+
+
 
 	}
-	
+
 	public static String getExactFileName(File f) {
 		String returnVal;
 		try {
@@ -187,15 +187,15 @@ public class FieldFixer {
 		}
 		return returnVal;
 	}
-	
+
 	private static void processThisRow(
-									   Map<String,Object> thisRow, 
-									   Shepherd myShepherd, 
-									   String splashImagesDirPath, 
-									   String encountersRootDirPath, 
-									   Table tSPLASHIDFilenames, 
+									   Map<String,Object> thisRow,
+									   Shepherd myShepherd,
+									   String splashImagesDirPath,
+									   String encountersRootDirPath,
+									   Table tSPLASHIDFilenames,
 									   String urlToThumbnailJSPPage,
-									   Table tSPLASHIDSexes, 
+									   Table tSPLASHIDSexes,
 									   Table tSightings,
 									   ArrayList<String> thumbnailThese,
 									   ArrayList<String> thumbnailTheseImages,
@@ -207,78 +207,78 @@ public class FieldFixer {
 									   TreeMap rgMap,
 									   Table tSampleLabData
 									   ){
-		
+
 		//create the encounter
 		String markedIndividualName=((Integer)thisRow.get("SPLASH ID")).toString().trim();
-		
+
 		boolean haveMadeChanges=false;
-		
+
 		myShepherd.beginDBTransaction();
-		
+
 		String IDKey=((Integer)thisRow.get("IDKey")).toString();
 		Encounter enc=myShepherd.getEncounter(IDKey);
 
-		
-		
+
+
 		/*
 		if(((String)thisRow.get("Sample Num")!=null)&&(!((String)thisRow.get("Sample Num")).trim().equals(""))){
-			
+
 			String sampleString=((String)thisRow.get("Sample Num"));
-			
-			
+
+
 			enc.setDynamicProperty("Tissue Sample", sampleString);
-			
-			
+
+
 		}
 		*/
-		
-		
+
+
 		enc.setMatchedBy("Contributor");
 		haveMadeChanges=true;
-	
+
 		//let's get what we can from tSightings
-		
+
 		/*
 		Iterator<Map<String,Object>> tSightingsIterator = tSightings.iterator();
 		//int numMatchingIdentifications=0;
 		while(tSightingsIterator.hasNext()){
 			Map<String,Object> thisSightRow=tSightingsIterator.next();
-			
-			
-			
-			
+
+
+
+
 		}
 		*/
-		
-		
+
+
 		//String imageName="";
 		//create its directory
 		//File encDir=new File(encountersRootDirPath, IDKey);
 		//if(!encDir.exists()){encDir.mkdir();}
-		
+
 		/*
 		Iterator<Map<String,Object>> tFlukeQualCodesIterator = tFlukeQualCodes.iterator();
 		String colorCode="";
 		while(tFlukeQualCodesIterator.hasNext()){
 			Map<String,Object> thisFlukeRow=tFlukeQualCodesIterator.next();
-			
+
 		}
-		
+
 	*/
 		if(haveMadeChanges){myShepherd.commitDBTransaction();}
 		else{myShepherd.rollbackDBTransaction();}
-		
-		
+
+
 	}
-	
-	
-	
-	
-	
 
-	
 
-	
-	
+
+
+
+
+
+
+
+
 
 }
