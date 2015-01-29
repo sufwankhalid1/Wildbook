c0d8766c1534759e2f6f0169b7d6c9956fe77b64
Jon Van Oast <jonv@scribble.com>
2015-01-29 00:53:03 +0000

    remove cruft we dont need now that we can cascade via rest

--- src/main/webapp/javascript/classes/Survey.js
+++ src/main/webapp/javascript/classes/Survey.js
@@ -4,15 +4,6 @@ wildbook.Model.Survey = wildbook.Model.BaseClass.extend({
 
 	classNameShort: function() { return 'survey.Survey'; },
 
-	refClass: {
-		tracks: 'SurveyTracks',
-	},
-
-	///TODO this is temporary til we figure out how to this nicerly
-	getTracks: function(callback) {
-		this.fetchSub('tracks', { jdoql: 'SELECT FROM org.ecocean.survey.SurveyTrack', success: callback });
-	}
-
 });
 
 
--- src/main/webapp/javascript/classes/SurveyTrack.js
+++ src/main/webapp/javascript/classes/SurveyTrack.js
@@ -1,18 +1,7 @@
 
 wildbook.Model.SurveyTrack = wildbook.Model.BaseClass.extend({
 
-
 	classNameShort: function() { return 'survey.SurveyTrack'; },
-
-	refClass: {
-		points: 'Points',
-	},
-
-	///TODO this is temporary til we figure out how to this nicerly
-	getPoints: function(callback) {
-		this.fetchSub('points', { jdoql: 'SELECT FROM org.ecocean.Point', success: callback });
-	}
-
 
 });
 
