304b0e3c4f6ad3acf828f0c6a4f0da0d145a87ac
Jon Van Oast <jonv@scribble.com>
2015-01-29 00:52:41 +0000

    some googly stuffs on Point, for testing

--- src/main/webapp/javascript/classes/Point.js
+++ src/main/webapp/javascript/classes/Point.js
@@ -1,6 +1,17 @@
 
 wildbook.Model.Point = wildbook.Model.BaseClass.extend({
 
+	placeGoogleMarker: function(gmap, title) {
+		return new google.maps.Marker({ map: gmap, position: this.toGoogleLatLng(), title: (title||'') });
+	},
+
+	centerGoogleMap: function(gmap) {
+		gmap.setCenter(this.toGoogleLatLng());
+	},
+
+	toGoogleLatLng: function() {
+		return new google.maps.LatLng(this.get('latitude'), this.get('longitude'));
+	}
 });
 
 
