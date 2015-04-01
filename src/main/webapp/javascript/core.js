b93ebe5e2c9ad86eaa33eb53a4a1c6d15ff24b95
Jon Van Oast <jon@apparentinc.com>
2015-04-01 16:20:05 -0700

    add wildbook.uuid() function to get use type 4 uuids in js-land

--- src/main/webapp/javascript/core.js
+++ src/main/webapp/javascript/core.js
@@ -133,6 +133,13 @@ console.log('is %o', ajax);
 		return obj.class.substr(12);
 	},
 
+	uuid: function() {   //  h/t http://stackoverflow.com/a/2117523/1525311
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    	var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    	return v.toString(16);
+		});
+	},
+
 	init: function(callback) {
 		classInit('Base', function() { wildbook.loadAllClasses(callback); });  //define base class first - rest can happen any order
 	}
