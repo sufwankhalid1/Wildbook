0d7560a23fd96d036b8cd892f3cc3aa5e8447425
Jon Van Oast <jon@apparentinc.com>
2015-06-19 15:43:33 -0700

    allow subdir to be passed for js SinglePhotoVideo.url(subdir)

--- src/main/webapp/javascript/classes/SinglePhotoVideo.js
+++ src/main/webapp/javascript/classes/SinglePhotoVideo.js
@@ -5,10 +5,17 @@ wildbook.Model.SinglePhotoVideo = wildbook.Model.BaseClass.extend({
 
 	//note: these assume .encounter is set -- which is only sure if we were loaded via .getImages() on parent encounter  TODO
 
-	url: function() {
-		return this.get('fullFileSystemPath').substr(wildbookGlobals.rootDir.length);
+	url: function(subdir) {
+		var u = this.get('fullFileSystemPath').substr(wildbookGlobals.rootDir.length);
+		if (subdir) {
+			var i = u.lastIndexOf('/');
+			if (i < 0) return u;
+			u = u.substr(0,i) + '/' + subdir + u.substr(i);
+		}
+		return u;
 	},
 
+	//note: urlSmall and urlMid (as currently written) only work for encounter-based paths!
 	urlSmall: function() {
 		return wildbookGlobals.dataUrl + '/encounters/' + this.subdir() + '/' + this.get('dataCollectionEventID') + '.jpg';
 	},
