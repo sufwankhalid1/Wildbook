ee11d0f642926bc72ccd2f96f9e0613f94c710ac
Jon Van Oast <jonv@scribble.com>
2015-06-23 19:37:34 +0000

    zooniverse export (beta)

---
+++ src/main/java/org/ecocean/servlet/export/ZooniverseImage.java
@@ -0,0 +1,61 @@
+package org.ecocean.servlet.export;
+
+/*
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+*/
+import java.io.IOException;
+import java.io.PrintWriter;
+/*
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+//import java.util.StringTokenizer;
+import java.util.Vector;
+*/
+import java.util.Iterator;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ecocean.*;
+import org.ecocean.servlet.ServletUtilities;
+
+public class ZooniverseImage extends HttpServlet{
+
+  public void init(ServletConfig config) throws ServletException {
+      super.init(config);
+    }
+
+  
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+      doPost(request, response);
+  }
+    
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //now write out the file
+        response.setContentType("text/plain");
+        //response.setHeader("Content-Disposition","attachment;filename="+inpFilename);
+        //ServletContext ctx = getServletContext();
+        //InputStream is = ctx.getResourceAsStream("/encounters/"+emailFilename);
+        PrintWriter out = response.getWriter();
+        String context="context0";
+        context=ServletUtilities.getContext(request);
+        Shepherd myShepherd = new Shepherd(context);
+
+        Iterator all_spv = myShepherd.getAllSinglePhotoVideosNoQuery();
+        while (all_spv.hasNext()) {
+            SinglePhotoVideo spv = (SinglePhotoVideo)all_spv.next();
+            out.println(spv.getDataCollectionEventID() + "\t" + spv.getFilename());
+        }
+        out.close(); 
+
+    }
+  
+}
