b08e79cb9771788764a48aba7ec9c1bb9695a6f9
crowmagnumb <crowmagnumb@gmail.com>
2015-07-10 11:09:43 -0700

    s6-db needs to be 1.0.3

---
+++ src/main/java/org/ecocean/rest/SimpleFactory.java
@@ -0,0 +1,96 @@
+package org.ecocean.rest;
+
+import org.ecocean.Encounter;
+import org.ecocean.MarkedIndividual;
+import org.ecocean.Shepherd;
+import org.ecocean.SinglePhotoVideo;
+
+public class SimpleFactory {
+    private SimpleFactory() {
+        // prevent instantiation
+    }
+
+
+    public static SimpleIndividual getIndividual(final String context,
+                                                 final String id) {
+        Shepherd myShepherd = new Shepherd(context);
+        MarkedIndividual mi = myShepherd.getMarkedIndividual(id);
+
+        if (mi == null) {
+            return null;
+        }
+
+        return getIndividual(context, mi);
+    }
+
+
+    public static SimpleIndividual getIndividual(final String context,
+                                                 final MarkedIndividual mi) {
+
+        SimpleIndividual ind = new SimpleIndividual(mi.getIndividualID(), mi.getNickName());
+        ind.setSex(mi.getSex());
+        //
+        // TODO: expose and fix thumbnail path if needed? Or just get from first image?
+        //
+//        ind.setThumbnail(mi.getThumbnail());
+
+        java.util.Iterator<Encounter> it = mi.getEncounters().iterator();
+        while (it.hasNext()) {
+            Encounter enc = it.next();
+            ind.addEncounter(getEncounter(context, enc));
+        }
+
+        //
+        // TODO: Have photos for individual be chosen from the "selected" or "highlighted"
+        // photos via a tag or whatever method we want the users to specify which are the best
+        // images. For now I'm just going to grab 8 or so images from all the encounters, grabbing
+        // the top images from each.
+        //
+        int count = 0;
+        int index = 0;
+        final int MAX_PHOTOS = 8;
+        for (SimpleEncounter encounter : ind.getEncounters()) {
+            int lastCount = Integer.MAX_VALUE;
+            while (count < MAX_PHOTOS && count != lastCount) {
+                lastCount = count;
+                if (encounter.getPhotos().size() > index) {
+                    ind.addPhoto(encounter.getPhotos().get(index));
+                    count++;
+                }
+            }
+        }
+
+        return ind;
+    }
+
+
+    public static SimpleEncounter getEncounter(final String context, final Encounter encounter)
+    {
+        SimpleEncounter se = new SimpleEncounter(encounter.getDateInMilliseconds());
+
+        se.setLocationid(encounter.getLocationID());
+        se.setVerbatimLocation(encounter.getLocation());
+        se.setLatitude(encounter.getLatitude());
+        se.setLongitude(encounter.getLongitude());
+
+        encounter.getSubmitterName();
+        for (SinglePhotoVideo photo : encounter.getSinglePhotoVideo())
+        {
+            se.addPhoto(getSimplePhoto(context, photo));
+        }
+
+        Shepherd myShepherd = new Shepherd(context);
+        se.setSubmitter(myShepherd.getUser(encounter.getSubmitterID()));
+
+        return se;
+    }
+
+
+    public static SimplePhoto getSimplePhoto(final String context,
+                                             final SinglePhotoVideo spv)
+    {
+        SimplePhoto sp = new SimplePhoto(spv.getDataCollectionEventID(),
+                                         spv.asUrl(context));
+        return sp;
+    }
+}
