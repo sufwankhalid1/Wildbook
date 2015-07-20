d89ef29c494b61f156a0c5bf6042c816e1d2fc94
Jon Van Oast <jon@apparentinc.com>
2015-07-20 12:49:18 -0700

    more voyage rest

--- src/main/java/org/ecocean/rest/SurveyTrackController.java
+++ src/main/java/org/ecocean/rest/SurveyTrackController.java
@@ -8,10 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.ecocean.ShepherdPMF;
 import org.ecocean.Shepherd;
+import org.ecocean.Util;
 import org.ecocean.SinglePhotoVideo;
 import org.ecocean.Point;
+import org.ecocean.Encounter;
+import org.ecocean.MarkedIndividual;
 import org.ecocean.servlet.ServletUtilities;
 import org.ecocean.survey.SurveyTrack;
+import org.ecocean.security.Stormpath;
 import org.ecocean.media.MediaSubmission;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,24 +37,28 @@ import com.samsix.database.SqlUpdateFormatter;
 import com.samsix.database.SqlWhereFormatter;
 import com.samsix.database.Table;
 
+import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.account.AccountList;
+import com.stormpath.sdk.client.Client;
+
 @RestController
 @RequestMapping(value = "/obj/surveytrack")
 public class SurveyTrackController
 {
 
-		public PersistenceManager getPM(final HttpServletRequest request) {
+    public PersistenceManager getPM(final HttpServletRequest request) {
         String context = "context0";
         context = ServletUtilities.getContext(request);
         Shepherd myShepherd = new Shepherd(context);
-				return myShepherd.getPM();
-		}
+                return myShepherd.getPM();
+    }
 
 /*  NOT sure if we ever need to save a track on its own?  see Survey appendTracks REST call
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ResponseEntity<SurveyTrack> save(final HttpServletRequest request,
                      @RequestBody @Valid final Survey survey) {
         System.out.println(survey);
-				getPM(request).makePersistent(survey);
+                getPM(request).makePersistent(survey);
         return new ResponseEntity<Survey>(survey, HttpStatus.OK);
     }
 */
@@ -61,24 +69,24 @@ public class SurveyTrackController
                                                     @RequestBody List<Point> points,
                                                     @PathVariable("surveyTrackId") final int surveyTrackId) {
 
-				PersistenceManager pm = getPM(request);
+        PersistenceManager pm = getPM(request);
         SurveyTrack track = null;
         try {
           track = (SurveyTrack) pm.getObjectById(SurveyTrack.class, surveyTrackId);
         } catch (Exception ex) {
-				}
+        }
 
-				if (track == null) {
-        	return new ResponseEntity<SurveyTrack>(track, HttpStatus.NOT_FOUND);
-				} else {
-					List<Point> prevPoints = track.getPoints();
-          /// TODO do we want also a SET (reset, not append) points?
-					if (prevPoints == null) prevPoints = new ArrayList<Point>();
-					prevPoints.addAll(points);
-					track.setPoints(prevPoints);
-					pm.makePersistent(track);
-        	return new ResponseEntity<SurveyTrack>(track, HttpStatus.OK);
-				}
+        if (track == null) {
+            return new ResponseEntity<SurveyTrack>(track, HttpStatus.NOT_FOUND);
+        } else {
+            List<Point> prevPoints = track.getPoints();
+            /// TODO do we want also a SET (reset, not append) points?
+            if (prevPoints == null) prevPoints = new ArrayList<Point>();
+            prevPoints.addAll(points);
+            track.setPoints(prevPoints);
+            pm.makePersistent(track);
+            return new ResponseEntity<SurveyTrack>(track, HttpStatus.OK);
+        }
     }
 
 
@@ -87,64 +95,159 @@ public class SurveyTrackController
                                                     @RequestBody List<SinglePhotoVideo> media,
                                                     @PathVariable("surveyTrackId") final int surveyTrackId) {
 
-				PersistenceManager pm = getPM(request);
+        PersistenceManager pm = getPM(request);
         SurveyTrack track = null;
         try {
           track = (SurveyTrack) pm.getObjectById(SurveyTrack.class, surveyTrackId);
         } catch (Exception ex) {
-				}
+        }
 
-				if (track == null) {
-        	return new ResponseEntity<SurveyTrack>(track, HttpStatus.NOT_FOUND);
-				} else {
-					List<SinglePhotoVideo> prevMedia = track.getMedia();
-					if (prevMedia == null) prevMedia = new ArrayList<SinglePhotoVideo>();
+        if (track == null) {
+            return new ResponseEntity<SurveyTrack>(track, HttpStatus.NOT_FOUND);
+        } else {
+            List<SinglePhotoVideo> prevMedia = track.getMedia();
+            if (prevMedia == null) prevMedia = new ArrayList<SinglePhotoVideo>();
 
-					//for some reason, media does not get populated such that when tag is persisted, it creates all new SPVs.. grrr wtf.. explicitely loading them fixes this
-					/////////prevMedia.addAll(media);
-					for (SinglePhotoVideo s : media) {
-      			SinglePhotoVideo obj = ((SinglePhotoVideo) (pm.getObjectById(pm.newObjectIdInstance(SinglePhotoVideo.class, s.getDataCollectionEventID()), true)));
-						if (obj != null) prevMedia.add(obj);
-					}
+            //for some reason, media does not get populated such that when tag is persisted, it creates all new SPVs.. grrr wtf.. explicitely loading them fixes this
+            /////////prevMedia.addAll(media);
+            for (SinglePhotoVideo s : media) {
+                SinglePhotoVideo obj = ((SinglePhotoVideo) (pm.getObjectById(pm.newObjectIdInstance(SinglePhotoVideo.class, s.getDataCollectionEventID()), true)));
+                if (obj != null) prevMedia.add(obj);
+            }
 
-					track.setMedia(prevMedia);
-					pm.makePersistent(track);
-        	return new ResponseEntity<SurveyTrack>(track, HttpStatus.OK);
-				}
+            track.setMedia(prevMedia);
+            pm.makePersistent(track);
+            return new ResponseEntity<SurveyTrack>(track, HttpStatus.OK);
+        }
     }
 
 
     //a "voyage" is based loosely upon a single SurveyTrack -- just with lots of other junk added.  for the voyage page specifically
     @RequestMapping(value = "/get/voyage/{id}", method = RequestMethod.GET)
-    public HashMap<String,Object> getSources(final HttpServletRequest request,
+    public HashMap<String,Object> getVoyage(final HttpServletRequest request,
                                             @PathVariable("id") final int id) throws DatabaseException {
+        return getVoyageObj(request, id);
+    }
+
+    //this version allows info tailored to a specific match
+    @RequestMapping(value = "/get/voyage/{id}/{matchMediaId}/{matchEncounterId}/{encounterMediaOffset}", method = RequestMethod.GET)
+    public HashMap<String,Object> getVoyage(final HttpServletRequest request,
+                                            @PathVariable("id") final int id,
+                                            @PathVariable("matchMediaId") final String matchMediaId,
+                                            @PathVariable("matchEncounterId") final String matchEncounterId,
+                                            @PathVariable("encounterMediaOffset") final int encounterMediaOffset
+                                           ) throws DatabaseException {
+        PersistenceManager pm = getPM(request);
+        String context = ServletUtilities.getContext(request);
+        HashMap<String,Object> obj = getVoyageObj(request, id);
+        //List<String> err = new ArrayList<String>();
+
+        SinglePhotoVideo testSpv = null;
+        try {
+            testSpv = (SinglePhotoVideo) pm.getObjectById(SinglePhotoVideo.class, matchMediaId);
+        } catch (Exception ex) {
+            obj.put("error", "could not find match image with id " + matchMediaId);
+            return obj;
+        }
+
+        Encounter enc = null;
+        try {
+            enc = (Encounter) pm.getObjectById(Encounter.class, matchEncounterId);
+        } catch (Exception ex) {
+            obj.put("error", "could not find match encounter with id " + matchEncounterId);
+            return obj;
+        }
+
+        List<SinglePhotoVideo> spvs = enc.getImages();
+        if ((spvs == null) || (spvs.size() < 1)) {
+            obj.put("error", "no images on encounter " + matchEncounterId);
+            return obj;
+        }
+        int offset = encounterMediaOffset;
+        if (offset >= spvs.size()) offset = 0;
+
+        MarkedIndividual ind = null;
+        try {
+            ind = (MarkedIndividual) pm.getObjectById(MarkedIndividual.class, enc.getIndividualID());
+        } catch (Exception ex) {
+            //we dont care (or do we?) if we dont have an individual -- would we ever even match against an unknown encounter? i guess.
+        }
+
+        //now we should have everything we need
+////TODO i18n
+        SurveyTrack track = (SurveyTrack)obj.get("surveyTrack");
+        Long trackTime = track.startTime();
+        Client client = Stormpath.getClient(ServletUtilities.getConfigDir(request));
+        HashMap<String,Object> match = new HashMap<String,Object>();
+        if (ind != null) match.put("individualID", ind.getIndividualID());
+        match.put("testImage", SimpleFactory.getPhoto(context, testSpv));
+        if (trackTime == null) {
+            match.put("testCaption", "Your photo taken from your " + track.getName() + " voyage.");
+        } else {
+            match.put("testCaption", "Your photo taken from your " + track.getName() + " voyage on " + trackTime + ".");
+        }
+        match.put("matchImage", SimpleFactory.getPhoto(context, spvs.get(offset)));
+
+        String photoUserName = enc.getSubmitterID();
+/////////TODO figure out how to properly attribute a photographer of an Encounter image !!! grrrr
+/*
+        AccountList accounts = Stormpath.getAccounts(client, enc.getSubmitterID());
+        if (accounts.getSize() > 0) {
+            Account account = accounts.iterator().next();
+            photoUserName = account.getFullName();
+        }
+*/
+        String mcaption = "Photo taken by " + photoUserName + " in " + enc.getYear();
+        if (!Util.isEmpty(enc.getLocationCode())) mcaption += " in " + enc.getLocationCode();
+        match.put("matchCaption", mcaption + ".");
+        if (ind == null) {
+            match.put("link", "unknown individual");
+        } else {
+            String name = ind.getIndividualID();
+            if (!ind.getNickName().equals("Unassigned")) name = ind.getNickName() + " (" + name + ")";
+            match.put("link", "<a href=\"/individual/" + ind.getIndividualID() + "\">" + name + "</a>");
+        }
+        
+        obj.put("match", match);
+
+        return obj;
+    }
+
+    private HashMap<String,Object> getVoyageObj(HttpServletRequest request, int id) {
         if (id < 1) return null;
         String context = "context0";
         context = ServletUtilities.getContext(request);
         PersistenceManager pm = getPM(request);
         SurveyTrack track = null;
         try {
-          track = (SurveyTrack) pm.getObjectById(SurveyTrack.class, id);
+            track = (SurveyTrack) pm.getObjectById(SurveyTrack.class, id);
         } catch (Exception ex) {
         }
         HashMap<String,Object> obj = new HashMap<String,Object>();
         obj.put("surveyTrack", track);
-/*
-        ConnectionInfo ci = ShepherdPMF.getConnectionInfo();
-        Database db = new Database(ci);
-        String sql = "SELECT \"DATACOLLECTIONEVENTID_EID\" AS mid FROM \"SURVEYTRACK_MEDIA\" WHERE \"ID_OID\"=" + id;
-System.out.println(sql);
-        RecordSet rs = db.getRecordSet(sql);
-        List<SinglePhotoVideo> media = new ArrayList<SinglePhotoVideo>();
-        while (rs.next()) {
-            SinglePhotoVideo spv = new SinglePhotoVideo();
-            spv.setDataCollectionEventID(rs.getString("mid"));
-            media.add(spv);
+        if (track != null) {
+            List<MediaSubmission> sources = MediaSubmission.findMediaSources(track.getMedia(), context);
+            if (sources.size() > 0) {
+                Client client = Stormpath.getClient(ServletUtilities.getConfigDir(request));
+                obj.put("sources", sources);
+                List<SimpleUser> contrib = new ArrayList<SimpleUser>();
+                for (MediaSubmission m : sources) {
+                    //(in theory) there should be a stormpath user for every mediasubmission; so we should get it via username or email
+                    String uname = m.getUsername();
+                    if (Util.isEmpty(uname)) uname = m.getEmail();
+System.out.println("using uname=" + uname);
+                    AccountList accounts = Stormpath.getAccounts(client, uname);
+                    if (accounts.getSize() > 0) {
+                        Account account = accounts.iterator().next();
+                        contrib.add(SimpleFactory.getStormpathUser(account));
+                    } else {
+System.out.println("could not find Stormpath user");
+                    }
+                }
+                obj.put("contributors", contrib);
+            }
         }
-        db.release();
-        return MediaSubmission.findMediaSources(media, context);
-*/
-        if (track != null) obj.put("sources", MediaSubmission.findMediaSources(track.getMedia(), context));
+
         return obj;
     }
 
