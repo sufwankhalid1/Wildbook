581888deaf492802842a07049438fe3830fbc898
crowmagnumb <crowmagnumb@gmail.com>
2015-08-15 22:14:38 -0700

    add other columns

--- src/main/webapp/javascript/media/mediaSubmitAdmin.js
+++ src/main/webapp/javascript/media/mediaSubmitAdmin.js
@@ -4,24 +4,44 @@ $(document).ready(function() {
         $("#mstable").DataTable({
             data: data,
             columns: [
-                      {data: 'id',
-                       title: 'ID'},
-                      {data: 'user',
-                       title: 'Submitted By',
-                       render: function(user) {
-                          if (user) {
-                              return user.displayName;
-                          }
-                          return null;
-                      }},
-                      {data: 'timeSubmitted',
-                       title: 'Date',
-                       render: function(timeSubmitted) {
-                           return moment(timeSubmitted).format('lll');
-                       }},
-                       {data: 'status',
-                        title: 'Status'}
-                  ]
+                {
+                    data: 'id',
+                    title: 'Edit'
+                },
+                {
+                    data: 'timeSubmitted',
+                    title: 'Submitted',
+                    render: function(timeSubmitted) {
+                        return moment(timeSubmitted).format('lll');
+                    }
+                },
+                {
+                    data: 'user',
+                    title: 'Submitted By',
+                    render: function(user) {
+                        if (user) {
+                            return user.displayName;
+                        }
+                        return null;
+                    }
+                },
+                {
+                    data: 'submissionid',
+                    title: "Survey ID"
+                },
+                {
+                    data: 'description',
+                    title: "Description"
+                },
+                {
+                    data: 'verbatimLocation',
+                    title: "Location"
+                },
+                {
+                    data: 'status',
+                    title: 'Status'
+                }
+                ]
         });
     }, function(error) {
         alertplus.error(error);
