2613b2c7c663d1a32fc05fec357e54e78cc015c5
crowmagnumb <crowmagnumb@gmail.com>
2015-08-15 23:25:37 -0700

    latest

--- src/main/webapp/javascript/media/mediaSubmitAdmin.js
+++ src/main/webapp/javascript/media/mediaSubmitAdmin.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     $.get("obj/mediasubmission/get/status")
     .then(function(data) {
-        $("#mstable").DataTable({
+        var table = $("#mstable").DataTable({
             data: data,
             columns: [
                 {
                     data: 'id',
-                    title: 'Edit'
+                    title: 'Edit',
+                    render: ''
                 },
                 {
                     data: 'timeSubmitted',
@@ -42,6 +43,32 @@ $(document).ready(function() {
                     title: 'Status'
                 }
                 ]
+        });
+
+//        var lastRow = null;
+//        $('#mstable tbody')
+//        .on( 'mouseover', 'tr', function () {
+//            if (lastRow = )
+//            lastRow =
+//            $(table.row(this)).addClass('highlight');
+//            var colIdx = table.cell(this).index().column;
+//
+//            if ( colIdx !== lastIdx ) {
+//                $( table.cells().nodes() ).removeClass( 'pageableTable-visible' );
+//                $( table.column( colIdx ).nodes() ).addClass( 'pageableTable-visible' );
+//            }
+//        } )
+//        .on( 'mouseleave', function () {
+//            $( table.cells().nodes() ).removeClass( 'pageableTable-visible' );
+//        } );
+
+        $("#mstable").on("click", "tr", function() {
+            var data = table.row(this).data();
+
+            //
+            // TODO: Trigger John's code for editing a mediasubmission
+            //
+            console.log(data);
         });
     }, function(error) {
         alertplus.error(error);
