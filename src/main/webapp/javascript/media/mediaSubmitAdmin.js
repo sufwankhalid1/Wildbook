bb78f7ab3ebf90bfb08c1476e97f5b62a8f8fe91
crowmagnumb <crowmagnumb@gmail.com>
2015-08-19 13:36:19 -0700

    Goodbye datatables hello angular grid. So much nicer.

--- src/main/webapp/javascript/media/mediaSubmitAdmin.js
+++ src/main/webapp/javascript/media/mediaSubmitAdmin.js
@@ -1,4 +1,4 @@
-angular.module('appWildbook', ["ui.bootstrap"])
+angular.module('appWildbook', ["ui.bootstrap", "angularGrid"])
 
 angular.module('appWildbook').controller('ModalZoomImageCtrl', function ($scope, $modalInstance, zoomimage) {
     $scope.zoomimage = zoomimage;
@@ -12,7 +12,7 @@ angular.module('appWildbook').controller("MediaSubmissionController", function (
     // See Modal section of http://angular-ui.github.io/bootstrap/ if you want to expand on this modal
     // code. Usage here is *extremely* basic.
     //
-    function thumbZoom(zoomimage) {
+    $scope.viewImage = function(url) {
         var modalInstance = $modal.open({
             animation: false,
             templateUrl: 'zoomimage.html',
@@ -20,7 +20,7 @@ angular.module('appWildbook').controller("MediaSubmissionController", function (
             size: "lg",
             resolve: {
                 zoomimage: function () {
-                    return zoomimage;
+                    return url;
                 }
             }
         });
@@ -30,7 +30,12 @@ angular.module('appWildbook').controller("MediaSubmissionController", function (
         alertplus.error(ex);
     }
 
-    function deleteImage(row, id) {
+    function deleteRows(gridOptions, filter) {
+        gridOptions.rowData = gridOptions.rowData.filter(filter);
+        gridOptions.api.onNewRows();
+    }
+
+    $scope.deleteImage = function(id) {
         return alertplus.confirm('Are you sure you want to delete this image?', "Delete Image", true)
         .then(function() {
             $.ajax({
@@ -40,10 +45,22 @@ angular.module('appWildbook').controller("MediaSubmissionController", function (
                 contentType: "application/json"
             })
             .then(function() {
-                row.remove().draw();
+                deleteRows($scope.imageGridOptions, function(value) {
+                    return (value.id !== id);
+                })
             }, handleError);
         });
     }
+
+    $scope.editSubmission = function(submission) {
+        return $http({url:"obj/mediasubmission/photos/" + submission.id})
+        .then(function(result) {
+            $scope.submission = submission;
+            $scope.imageGridOptions.rowData = result.data;
+            $scope.imageGridOptions.api.onNewRows();
+        }, handleError);
+    }
+
 
     $scope.deleteSubmission = function() {
         return alertplus.confirm('Are you sure you want to delete the <b>entire</b> submission?', "Delete Submission", true)
@@ -56,148 +73,85 @@ angular.module('appWildbook').controller("MediaSubmissionController", function (
             })
             .then(function() {
                 $scope.$apply(function() {
+                    deleteRows($scope.msGridOptions, function(value) {
+                        return (value.id !== $scope.submission.id);
+                    });
+
                     $scope.submission = null;
-                    $scope.currentRow.remove().draw();
-                    $scope.currentRow = null;
                 })
             }, handleError);
         });
     }
 
-    var msTable = $("#mstable").DataTable({
-        select: {style: "os"},
-        columns: [
-                  {
-                      data: 'id',
-                      title: 'Edit',
-                      render: function(id, type, data, meta) {
-                          return '<a href="javascript:;"><i class="glyphicon glyphicon-edit"></i></a>';
-                      },
-                      width: "32px"
-                  },
-                  {
-                      data: 'timeSubmitted',
-                      title: 'Submitted',
-                      render: function(timeSubmitted) {
-                          return moment(timeSubmitted).format('lll');
-                      }
-                  },
-                  {
-                      data: 'user',
-                      title: 'Submitted By',
-                      render: function(user) {
-                          if (user) {
-                              return user.displayName;
-                          }
-                          return null;
-                      }
-                  },
-                  {
-                      data: 'submissionid',
-                      title: "Survey ID"
-                  },
-                  {
-                      data: 'description',
-                      title: "Description"
-                  },
-                  {
-                      data: 'verbatimLocation',
-                      title: "Location"
-                  },
-                  {
-                      data: 'status',
-                      title: 'Status'
-                  }
-                  ]
-    });
 
-    var imageTable = $("#imagetable").DataTable({
-        columns: [
-                  {
-                      data: 'id',
-                      title: 'Delete',
-                      render: function(id, type, data, meta) {
-                          //
-                          // Can't get this to work. So resorting to click event below.
-                          //
-//                          var content = '<a href="javascript:;" ng-click="deleteImage('
-//                              + meta.row
-//                              + ", '"
-//                              + id
-//                              + '\')"><i class="glyphicon glyphicon-trash"></i></a>';
-//                          content = "{{submission.id}}";
-//                          //
-//                          // Now compile this with angular so that ng-click is understood.
-//                          //
-//                          return $compile(content)($scope)[0].outerHTML;
-                          return '<a href="javascript:;"><i class="glyphicon glyphicon-trash"></i></a>';
-                      },
-                      width: "32px"
-                  },
-                  {
-                      data: 'thumbUrl',
-                      title: 'Image',
-                      render: function(thumb) {
-                          // the ../ is for now until we get actual address?
-                          return '<a href="javascript:;"><img width="50" src="..' + thumb + '"></a>';
-                      }
-                  }
-                  ]
-    });
+    $scope.msGridOptions = {
+        columnDefs:
+            [
+             {
+                 headerName: "Edit",
+                 field: "id",
+                 width: 32,
+                 template: '<a href="javascript:;" ng-click="editSubmission(data)"><i class="glyphicon glyphicon-edit"></i></a>'
+             },
+             {
+                 headerName: "Submitted",
+                 field: "timeSubmitted",
+                 cellRenderer: function(params) {
+                     return moment(params.value).format('lll');
+                 }
+             },
+             {
+                 headerName: "Submitted By",
+                 field: "user",
+                 cellRenderer: function(params) {
+                     if (params.value) {
+                         return params.value.displayName;
+                     }
+                     return null;
+                 }
+             },
+             {headerName: "Survey ID", field: "submissionid"},
+             {headerName: "Description", field: "description"},
+             {headerName: "Location", field: "verbatimLocation"},
+             {headerName: "Status", field: "status"}
+             ],
+        rowData: null,
+        enableSorting: true,
+        angularCompileRows: true
+    };
 
-    $("#imagetable").on("click", "td", function(evt) {
-        //
-        // To get row info from cell
-        //    imageTable.row(imageTable.cell(this).index().row).data();
-        //
-        // To get the number of rows in the table you can do.
-        //    imageTable.data().length
-        //
-        var cell = imageTable.cell(this);
-        if (!cell.index()) {
-            return;
-        }
+    $scope.imageGridOptions = {
+        columnDefs:
+            [
+             {
+                 field: 'id',
+                 headerName: 'Delete',
+                 template: '<a href="javascript:;" ng-click="deleteImage(data.id)"><i class="glyphicon glyphicon-trash"></i></a>',
+                 width: 32
+             },
+             {
+                 field: 'thumbUrl',
+                 headerName: 'Image',
+                 template: '<a href="javascript:;" ng-click="viewImage(data.url)"><img width="50px" src="..{{data.thumbUrl}}"></a>'
+             }
+             ],
+             rowData: null,
+             rowHeight: 50,
+             enableSorting: true,
+             angularCompileRows: true
+    };
 
-        var rowindex = cell.index().row;
-        var row = imageTable.row(rowindex);
-        switch (cell.index().column) {
-        case 0:
-            deleteImage(row, cell.data());
-            break;
-        case 1:
-            thumbZoom(row.data().url);
-            break;
-        }
-    });
-
-    $("#mstable").on("click", "td", function() {
-        var cell = msTable.cell(this);
-        if (cell.index() && cell.index().column == 0) {
-            $scope.currentRow = msTable.row(cell.index().row);
-            $scope.submission = $scope.currentRow.data();
-            //
-            // This is the code if we add the click event to the <tr> element.
-            //
-            //$scope.currentRow = msTable.row(this);
-
-            return $http({url:"obj/mediasubmission/photos/" + $scope.submission.id})
-            .then(function(images) {
-                imageTable.clear();
-                imageTable.rows.add(images.data).draw();
-            }, handleError);
-        }
-    });
 
     $scope.doneEditing = function() {
         $scope.submission = null;
-        $scope.currentRow = null;
     };
 
     //
     // Finally, kick us off.
     //
     return $http({url:"obj/mediasubmission/get/status"})
-    .then(function(msResult) {
-        msTable.rows.add(msResult.data).draw();
+    .then(function(result) {
+        $scope.msGridOptions.rowData = result.data;
+        $scope.msGridOptions.api.onNewRows();
     }, handleError);
 });
