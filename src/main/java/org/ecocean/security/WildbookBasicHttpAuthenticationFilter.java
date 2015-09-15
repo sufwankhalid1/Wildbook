6f4c647050bc6827eb70e03663a6bbe9183fa6c3
crowmagnumb <crowmagnumb@gmail.com>
2015-09-15 14:18:51 -0700

    whitespace

--- src/main/java/org/ecocean/security/WildbookBasicHttpAuthenticationFilter.java
+++ src/main/java/org/ecocean/security/WildbookBasicHttpAuthenticationFilter.java
@@ -11,25 +11,23 @@ import org.ecocean.servlet.ServletUtilities;
 import com.samsix.database.Database;
 import com.samsix.database.DatabaseException;
 
-public class WildbookBasicHttpAuthenticationFilter extends
-    BasicHttpAuthenticationFilter {
-
-
-@Override
-protected AuthenticationToken createToken(final ServletRequest request, final ServletResponse response) {
+public class WildbookBasicHttpAuthenticationFilter
+        extends BasicHttpAuthenticationFilter {
+    @Override
+    protected AuthenticationToken createToken(final ServletRequest request, final ServletResponse response) {
         String authorizationHeader = getAuthzHeader(request);
         if (authorizationHeader == null || authorizationHeader.length() == 0) {
             // Create an empty authentication token since there is no
-           // Authorization header.
+            // Authorization header.
             return createToken("", "", request, response);
-       }
+        }
 
         String[] prinCred = getPrincipalsAndCredentials(authorizationHeader, request);
         if (prinCred == null || prinCred.length < 2) {
-           // Create an authentication token with an empty password,
+            // Create an authentication token with an empty password,
             // since one hasn't been provided in the request.
             String username = prinCred == null || prinCred.length == 0 ? "" : prinCred[0];
-           return createToken(username, "", request, response);
+            return createToken(username, "", request, response);
         }
 
         String username = prinCred[0];
@@ -59,8 +57,5 @@ protected AuthenticationToken createToken(final ServletRequest request, final Se
             //
             return createToken(username, ServletUtilities.hashAndSaltPassword(password, ""), request, response);
         }
-  }
-
-
-
+    }
 }
