697392be87233ec8e98002aea4f0f5407c568f33
crowmagnumb <crowmagnumb@gmail.com>
2015-09-16 23:39:34 -0700

    checking singleton

--- src/main/java/org/ecocean/Global.java
+++ src/main/java/org/ecocean/Global.java
@@ -1,20 +1,29 @@
 package org.ecocean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.samsix.util.io.ResourceReader;
 import com.samsix.util.io.ResourceReaderImpl;
 
 public enum Global {
     INST;
 
+    private static Logger logger = LoggerFactory.getLogger(Global.class);
+
     private ResourceReader initResources;
 
-    public void setInitResources(final ResourceReader initResources) {
-        this.initResources = initResources;
+    public void setInitResources(final ResourceReader resources) {
+        initResources = resources;
     }
 
     public ResourceReader getInitResources() {
-        if (initResources != null) {
-            this.initResources = new ResourceReaderImpl();
+        if (initResources == null) {
+            //
+            // Purely as a safe-guard. Should not happen.
+            //
+            initResources = new ResourceReaderImpl();
+            logger.warn("Init Resources were null, setting to empty set.");
         }
 
         return initResources;
