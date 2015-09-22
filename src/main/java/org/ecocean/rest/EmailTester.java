cac73737c736a46892af5362c48542b040644fc3
crowmagnumb <crowmagnumb@gmail.com>
2015-09-22 11:01:22 -0700

    adding user object to email tester

--- src/main/java/org/ecocean/rest/EmailTester.java
+++ src/main/java/org/ecocean/rest/EmailTester.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.ecocean.email.EmailUtils;
+import org.ecocean.security.User;
+import org.ecocean.security.UserFactory;
 import org.ecocean.servlet.ServletUtilities;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,17 +36,24 @@ public class EmailTester {
         Map<String, Object> model = new HashMap<String, Object>();
 
         try (Database db = ServletUtilities.getDb(request)) {
-            //
-            // TODO: Add other basic things like encounters and what-not
-            //
             String individualId = request.getParameter("individualid");
             if (individualId != null) {
                 SimpleIndividual ind = SimpleFactory.getIndividual(db, Integer.parseInt(individualId));
-                model.put("individual", ind);
+                if (ind != null) {
+                    model.put("individual", ind);
+                }
             }
 
-            PrintWriter out = response.getWriter();
-            out.println(EmailUtils.getJadeEmailBody(template, model));
+            String userId = request.getParameter("userid");
+            if (userId != null) {
+                User user = UserFactory.getUserById(db, Integer.parseInt(userId));
+                if (user !=null) {
+                    model.put("user", user);
+                }
+            }
         }
+
+        PrintWriter out = response.getWriter();
+        out.println(EmailUtils.getJadeEmailBody(template, model));
     }
 }
