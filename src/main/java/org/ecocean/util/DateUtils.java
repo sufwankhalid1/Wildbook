56ffb3603b1c6831b18e3bb5460aa049f89e5cf5
crowmagnumb <crowmagnumb@gmail.com>
2015-09-24 14:54:54 -0700

    fix date string

--- src/main/java/org/ecocean/util/DateUtils.java
+++ src/main/java/org/ecocean/util/DateUtils.java
@@ -3,13 +3,23 @@ package org.ecocean.util;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
     private DateUtils() {
         // prevent instantiation
     }
 
-    public static LocalDateTime ofEpochSecond(final long epochSecond) {
+    public static LocalDateTime epochSecToLDT(final long epochSecond) {
         return LocalDateTime.ofInstant(Instant.ofEpochSecond(epochSecond), ZoneId.systemDefault());
+    }
+
+    /**
+     * @param epochSecond seconds since the epoch
+     * @return date as string (e.g. "1986-04-08 12:30")
+     */
+    public static String epochSecToString(final long epochSecond) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return epochSecToLDT(epochSecond).format(formatter);
     }
 }
