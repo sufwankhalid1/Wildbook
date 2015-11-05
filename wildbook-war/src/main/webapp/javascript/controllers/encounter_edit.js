8bc51fc7ee245db4cd987e8b50331da56ba3b6ad
crowmagnumb <crowmagnumb@gmail.com>
2015-11-04 23:47:28 -0800

    cleanup. Less wordy.

--- wildbook-war/src/main/webapp/javascript/controllers/encounter_edit.js
+++ wildbook-war/src/main/webapp/javascript/controllers/encounter_edit.js
@@ -1,10 +1,10 @@
 wildbook.app.directive(
     'wbEncounterEdit',
-    ["wbConfig", function($scope, $http, $exceptionHandler, wbConfig) {
+    function() {
         return {
             restrict: 'E',
             scope: {
-                encounterToEdit: "@encounter",
+                encounter: "=encounterToEdit",
                 editEncounterDone: "&"
             },
             templateUrl: 'util/render?j=partials/encounter_edit',
@@ -43,50 +43,5 @@ wildbook.app.directive(
                 }
             }
         };
-    }]
+    }
 );
-
-
-
-
-
-//wildbook.app.controller("EncounterEditController", function($scope, $http, $exceptionHandler, wbConfig) {
-//    var panelName = "encounter_edit";
-//
-//    $scope.$on(panelName, function(event, data) {
-//        if (data) {
-//            $scope.encounter = data;
-//        } else {
-//            $scope.encounter = {individual: {species: wbConfig.config().species[0]}};
-//        }
-//    });
-//    
-//    function closePanel() {
-//        $scope.panels[panelName] = false;
-//        $scope.$emit(panelName + "_done", $scope.encounter);
-//    }
-//
-//    $scope.getSpecies = function() {
-//        return wbConfig.config().species;
-//    }
-//    
-//    $scope.save = function() {
-//        $http.post('obj/encounter/save', $scope.encounter)
-//        .then(function(result) {
-//            $scope.encounter.id = result.data;
-//            closePanel();
-//        }, $exceptionHandler);
-//    };
-//    
-//    //
-//    // wb-key-handler-form
-//    //
-//    $scope.cancel = function() {
-//        $scope.encounter = null;
-//        closePanel();
-//    }
-//    
-//    $scope.cmdEnter = function() {
-//        $scope.save();
-//    }
-//});
