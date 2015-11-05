94ba51f554b22e0275c8d9945245f4094b1fbe81
crowmagnumb <crowmagnumb@gmail.com>
2015-11-05 15:09:16 -0800

    Can now pan in single image mode

--- wildbook-war/src/main/webapp/javascript/controllers/main.js
+++ wildbook-war/src/main/webapp/javascript/controllers/main.js
@@ -210,6 +210,8 @@ wildbook.app.directive(
             replace: true,
             controller($scope) {
                 var startIdx = 0;
+                var idx;
+                
                 var numPhotos;
                 if (! $scope.numPhotos) {
                     numPhotos = 18;
@@ -221,17 +223,37 @@ wildbook.app.directive(
                     return wbDateUtils.dateStringFromRest(photo.timestamp);
                 }
                 
-                $scope.panLeft = function() {
+                function pageLeft() {
                     startIdx = startIdx - numPhotos;
                     if (startIdx < 0) {
                         startIdx = 0;
                     }
                 }
-
-                $scope.panRight = function() {
+                
+                $scope.panLeft = function() {
                     if (! $scope.photos) {
                         return;
                     }
+                    if ($scope.zoomimage) {
+                        if (idx <= 0) {
+                            return;
+                        }
+                        idx--;
+                        $scope.zoomimage = $scope.photos[idx];
+                        
+                        //
+                        // If we have panned far enough such that we are now on the next
+                        // "page" of thumbnails, let's reflect that.
+                        //
+                        if (idx < startIdx) {
+                            pageLeft();
+                        }
+                    } else {
+                        pageLeft();
+                    }
+                }
+
+                function pageRight() {
                     startIdx = startIdx + numPhotos;
                     if (startIdx + numPhotos > $scope.photos.length) {
                         startIdx = $scope.photos.length - numPhotos;
@@ -241,11 +263,41 @@ wildbook.app.directive(
                     }
                 }
                 
+                $scope.panRight = function() {
+                    if (! $scope.photos) {
+                        return;
+                    }
+                    if ($scope.zoomimage) {
+                        if (idx >= $scope.photos.length - 1) {
+                            return;
+                        }
+                        idx++;
+                        $scope.zoomimage = $scope.photos[idx];
+                        
+                        //
+                        // If we have panned far enough such that we are now on the next
+                        // "page" of thumbnails, let's reflect that.
+                        //
+                        if (idx >= startIdx + numPhotos) {
+                            pageRight();
+                        }
+                    } else {
+                        pageRight();
+                    }
+                }
+                
                 $scope.isLeftDisabled = function() {
+                    if ($scope.zoomimage) {
+                        return (idx <= 0);
+                    }
                     return (startIdx <= 0);
                 }
                 
                 $scope.isRightDisabled = function() {
+                    if ($scope.zoomimage) {
+                        return (idx >= $scope.photos.length - 1);
+                    }
+                    
                     if (! $scope.photos) {
                         return true;
                     }
@@ -254,6 +306,12 @@ wildbook.app.directive(
                 
                 $scope.viewImage = function(photo) {
                     $scope.zoomimage = photo;
+                    
+                    $scope.photos.forEach(function(value, index) {
+                        if (value.id === photo.id) {
+                            idx = index;
+                        }
+                    });
                 }
                 
                 $scope.getVisPhotos = function() {
