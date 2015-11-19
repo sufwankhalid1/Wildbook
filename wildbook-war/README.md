e08998a1d7025a66cb45aff70d3c827908744734
crowmagnumb <crowmagnumb@gmail.com>
2015-11-19 15:39:17 -0800

    Don't need updatewar shell script now that we have gulp

--- wildbook-war/README.md
+++ wildbook-war/README.md
@@ -2,7 +2,9 @@
 
 #Angular Jade Templates
 ##Setup
-    npm install --global gulp
+Should be nothing to do as all the appropriate dev dependencies should be in package.json. To get them in there I ran the following commands (but again you shouldn't have to do this).
+
+    npm install -g gulp
     npm install gulp --save-dev
     npm install gulp-jade --save-dev
     npm install gulp-angular-templatecache --save-dev
--- wildbook-war/gulpfile.js
+++ wildbook-war/gulpfile.js
@@ -1,8 +1,29 @@
+//
+// To add a new library to the package.json file run
+// e.g. npm install gulp-rsync --save-dev
+//
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var templateCache = require('gulp-angular-templatecache');
 var less = require('gulp-less');
-var watch = require('gulp-watch');
+var rsync = require('rsyncwrapper').rsync;
+var argv = require('yargs').argv;
+var debug = require('gulp-debug');
+var run = require('gulp-run');
+
+var webapp = argv.w || argv.webapp || "wildbook";
+
+function doRsync(opts) {
+    opts.recursive = true;
+    
+    rsync(opts,
+        function(error, stdout, stderr, cmd) {
+            if (error) {
+                console.log(error.message);
+            }
+        }
+    );
+}
 
 gulp.task('templates', function() {
     return gulp.src('src/main/templates/**/*.jade').pipe(jade())
@@ -14,6 +35,38 @@ gulp.task('less', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/main/webapp/less/*.less', ['less']);
-    gulp.watch('src/main/templates/**/*.jade', ['templates']);
+    gulp.watch(['src/main/webapp/less/*.less', 'src/main/templates/**/*.jade'], ['updatewar']);
+    //gulp.watch('src/main/templates/**/*.jade', ['templates']);
+});
+
+gulp.task('updatewar', ['less', 'templates'], function() {
+    if (!process.env.TOMCAT_HOME) {
+        throw "Must have TOMCAT_HOME env variable set.";
+    }
+    
+    doRsync({
+        src: ['src/main/webapp/css', 'src/main/webapp/images', 'src/main/webapp/jade', 'src/main/webapp/javascript'],
+        dest: process.env.TOMCAT_HOME + '/webapps/' + webapp
+    });
+});
+
+gulp.task('updatewarclasses', function() {
+    var cmd = 'mvn compile -o';
+    var cust = argv.c || argv.cust;
+    if (cust) {
+        cmd += ' -Dwildbook.cust=' + cust;
+    }
+    run(cmd);
+    
+    doRsync({
+        src: ['target/classes'],
+        dest: process.env.TOMCAT_HOME + '/webapps/' + webapp + '/WEB-INF/classes'
+    });
+});
+
+gulp.task('updatewartools', function() {
+    doRsync({
+        src: ['src/main/webapp/tools', 'src/main/webapp/bcomponents'],
+        dest: process.env.TOMCAT_HOME + '/webapps/' + webapp
+    });
 });
