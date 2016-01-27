7642cf015aff3876f59f5c47654ae3e4e2efd116
Justin <justin.hallquist1@gmail.com>
2016-01-26 16:15:59 -0800

    autofocus directive, style/autofocus added to org and species page

--- wildbook-war/src/main/javascript/util/design_util.js
+++ wildbook-war/src/main/javascript/util/design_util.js
@@ -12,4 +12,20 @@ angular.module('wildbook.util')
                 },
         template: '<md-icon md-svg-icon="close-circle" ng-show="!hideme" ng-click="cancel()"><md-tooltip>Cancel</md-tooltip></md-icon>'
     };
+}])
+.directive('autoFocus', ['$timeout', '$window', function($timeout, $window){
+    return {
+        restrict: 'A',
+        link: function($scope, ele, attr){
+            $scope.autofocus = function(id) {
+                $timeout(function(){
+                    var element = $window.document.getElementById(id);
+                    console.log(element);
+                    if (element) {
+                      element.focus();
+                    }
+                });
+            };
+        },
+    };
 }]);
