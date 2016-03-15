58e42a58eb8e5d2cd4d246caa7c9e323bd311f6d
crowmagnumb <crowmagnumb@gmail.com>
2016-03-15 09:35:43 -0700

    not sure how this got mangled in the rebase develop

--- wildbook-core/src/main/java/org/ecocean/event/NullEventHandler.java
+++ wildbook-core/src/main/java/org/ecocean/event/NullEventHandler.java
@@ -2,7 +2,7 @@ package org.ecocean.event;
 
 public class NullEventHandler implements EventHandler {
     @Override
-    public void trigger(final String type, final Object relatedObj) {
+    public void trigger(final BaseEvent event) {
         // do nothing
     }
 }
