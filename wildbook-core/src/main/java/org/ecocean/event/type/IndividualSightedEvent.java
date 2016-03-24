03443501e578ad18e50aa87c54c525c21e357a74
crowmagnumb <crowmagnumb@gmail.com>
2016-03-24 11:48:26 -0700

    Add debug api call for checking the store

--- wildbook-core/src/main/java/org/ecocean/event/type/IndividualSightedEvent.java
+++ wildbook-core/src/main/java/org/ecocean/event/type/IndividualSightedEvent.java
@@ -2,20 +2,19 @@ package org.ecocean.event.type;
 
 import java.time.OffsetDateTime;
 
+import org.ecocean.encounter.SimpleEncounter;
 import org.ecocean.event.BaseEvent;
-import org.ecocean.rest.SimpleIndividual;
 
 public class IndividualSightedEvent extends BaseEvent {
     public final static String EVENT_NAME = "ind_sighted";
 
-    public IndividualSightedEvent(final SimpleIndividual ind) {
-        super(EVENT_NAME, ind, ind.getId());
+    public IndividualSightedEvent(final SimpleEncounter enc) {
+        super(EVENT_NAME, enc, enc.getId());
     }
 
     public IndividualSightedEvent(final int eventId,
                                   final OffsetDateTime timestamp,
-                                  final SimpleIndividual ind) {
-        super(eventId, EVENT_NAME, timestamp, ind, ind.getId());
+                                  final SimpleEncounter enc) {
+        super(eventId, EVENT_NAME, timestamp, enc, enc.getId());
     }
-
 }
