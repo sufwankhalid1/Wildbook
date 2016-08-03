61032693a2b84994d0ec2f17eeaebf34827103a6
drewblount <me@drew.computer>
2016-08-03 14:54:25 -0700

    full first version of ClassEditTemplate

--- src/main/java/org/ecocean/ClassEditTemplate.java
+++ src/main/java/org/ecocean/ClassEditTemplate.java
@@ -10,8 +10,10 @@ import org.joda.time.DateTime;
 import org.joda.time.LocalDateTime;
 
 /*
- * An almost entirely public static / functional class for printing html UI elements
- * for editing class fields on pages such as occurrence.jsp.
+ * An almost entirely static / functional class for printing html UI elements
+ * for editing class fields on pages such as occurrence.jsp. Note that this
+ * class works only in conjunction with webapp/css/classEditTemplate.css
+ * and webapp/javascript/classEditTemplate.js
  */
 
 public class ClassEditTemplate {
@@ -27,38 +29,38 @@ public class ClassEditTemplate {
         Double dbl = Double.parseDouble(valueAsString);
         Method setter = obj.getClass().getMethod(setterName, Double.class);
         setter.invoke(obj, dbl);
-        System.out.println("objURRENCE.JSP: just invoked "+setterName+" with value "+dbl);
+        System.out.println("updateObjectField: just invoked "+setterName+" with value "+dbl);
       }
 
       if (c == Integer.class){
         Integer in = Integer.parseInt(valueAsString);
         Method setter = obj.getClass().getMethod(setterName, Integer.class);
         setter.invoke(obj, in);
-        System.out.println("objURRENCE.JSP: just invoked "+setterName+" with value "+in);
+        System.out.println("updateObjectField: just invoked "+setterName+" with value "+in);
       }
 
       if (c == Boolean.class){
         Boolean bo = Boolean.parseBoolean(valueAsString);
         Method setter = obj.getClass().getMethod(setterName, Boolean.class);
         setter.invoke(obj, bo);
-        System.out.println("objURRENCE.JSP: just invoked "+setterName+" with value "+bo);
+        System.out.println("updateObjectField: just invoked "+setterName+" with value "+bo);
       }
 
       if (c == String.class){
         Method setter = obj.getClass().getMethod(setterName, String.class);
         setter.invoke(obj, valueAsString);
-        System.out.println("objURRENCE.JSP: just invoked "+setterName+" with value "+valueAsString);
+        System.out.println("updateObjectField: just invoked "+setterName+" with value "+valueAsString);
       }
 
       if (c == DateTime.class){
         DateTime dt = DateTime.parse(valueAsString);
         Method setter = obj.getClass().getMethod(setterName, DateTime.class);
         setter.invoke(obj, dt);
-        System.out.println("objURRENCE.JSP: just invoked "+setterName+" with value "+dt);
+        System.out.println("updateObjectField: just invoked "+setterName+" with value "+dt);
 
       }
     } catch (Exception e) {
-      System.out.println("objURRENCE.JSP: was not able to invoke "+setterName+" with value "+valueAsString);
+      System.out.println("updateObjectField: was not able to invoke "+setterName+" with value "+valueAsString);
       e.printStackTrace();
     }
   }
@@ -84,9 +86,9 @@ public class ClassEditTemplate {
       java.lang.reflect.Method getter = occ.getClass().getMethod(getterName);
       int oldVal = (Integer) getter.invoke(occ);
       boolean result = (oldVal == newVal);
-      System.out.println("OCCURRENCE.JSP: "+getterName+" = "+oldVal);
-      System.out.println("              : newVal = "+newVal);
-      System.out.println("              : result = "+result);
+      System.out.println("ClassEditTemplate: "+getterName+" = "+oldVal);
+      System.out.println("                 : newVal = "+newVal);
+      System.out.println("                 : result = "+result);
       return result;
     } catch (Exception e) {
       System.out.println("exception caught");
@@ -153,16 +155,16 @@ public class ClassEditTemplate {
     String fieldName = prettyFieldNameFromGetMethod(getDateTime);
     String inputName = inputElemName(getDateTime, classNamePrefix);
 
-    System.out.println("<tr data-original-value=\""+printValue+"\">");
-    System.out.println("\t<td>"+fieldName+"</td>");
-    System.out.println("\t<td>");
+    out.println("<tr data-original-value=\""+printValue+"\">");
+    out.println("\t<td>"+fieldName+"</td>");
+    out.println("\t<td>");
     // hidden input for setting default va a la http://stackoverflow.com/a/11904956
     //out.println("\t\t<input type=\"hidden\" id=\"datepicker\" />");
-    System.out.println("<input class=\"form-control\" type=\"text\" id=\"datepicker\"");
-    System.out.println("name=\""+inputName+"\" ");
-    System.out.println("value=\""+printValue+"\"");
-    System.out.println("/>");
-    System.out.println("\t</td>");
+    out.println("<input class=\"form-control\" type=\"text\" id=\"datepicker\"");
+    out.println("name=\""+inputName+"\" ");
+    out.println("value=\""+printValue+"\"");
+    out.println("/>");
+    out.println("\t</td>");
 
 
   }
@@ -184,24 +186,24 @@ public class ClassEditTemplate {
 
 
 
-    System.out.println("<tr data-original-value=\""+printValue+"\">");
-    System.out.println("\t<td>"+fieldName+"</td>");
-    System.out.println("\t<td>");
-    System.out.println("\t\t<input ");
-    System.out.println("name=\""+inputName+"\" ");
-    System.out.println("value=\""+printValue+"\"");
-    System.out.println("/>");
-    System.out.println("\t</td>");
+    out.println("<tr data-original-value=\""+printValue+"\">");
+    out.println("\t<td>"+fieldName+"</td>");
+    out.println("\t<td>");
+    out.println("\t\t<input ");
+    out.println("name=\""+inputName+"\" ");
+    out.println("value=\""+printValue+"\"");
+    out.println("/>");
+    out.println("\t</td>");
 
 
 
-    System.out.println("<td class=\"undo-container\">");
-    System.out.println("<div title=\"undo this change\" class=\"undo-button\">&#8635;</div>");
-    System.out.println("</td>");
+    out.println("<td class=\"undo-container\">");
+    out.println("<div title=\"undo this change\" class=\"undo-button\">&#8635;</div>");
+    out.println("</td>");
 
 
 
-    System.out.println("\n</tr>");
+    out.println("\n</tr>");
   }
 
   public static void printUnmodifiableField(Object obj, Method getMethod, javax.servlet.jsp.JspWriter out) throws IOException, IllegalAccessException, InvocationTargetException {
@@ -219,10 +221,10 @@ public class ClassEditTemplate {
 
 
 
-    System.out.println("\n<tr>");
-    System.out.println("\n\t<td>"+fieldName+"</td>");
-    System.out.println("\n\t<td>"+printValue+"</td>");
-    System.out.println("\n</tr>");
+    out.println("\n<tr>");
+    out.println("\n\t<td>"+fieldName+"</td>");
+    out.println("\n\t<td>"+printValue+"</td>");
+    out.println("\n</tr>");
   }
 
 
