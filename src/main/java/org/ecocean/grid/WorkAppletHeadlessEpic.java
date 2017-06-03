7f0c0e06401f9f1038731b57df806e019df55170
WildMeOrg <services@wildme.org>
2017-06-02 23:36:11 -0400

    LOCAL: narrowing matching to just this server

--- src/main/java/org/ecocean/grid/WorkAppletHeadlessEpic.java
+++ src/main/java/org/ecocean/grid/WorkAppletHeadlessEpic.java
@@ -87,10 +87,8 @@ public class WorkAppletHeadlessEpic {
 
   public static void main(String args[]) {
 
-    urlArray.add("https://www.whaleshark.org");
-    urlArray.add("http://www.spotashark.com");
-    // IP for Bass Server
-    urlArray.add("http://34.209.17.78");
+    urlArray.add("http://sevengill.oceansanctuaries.org");
+
 
     WorkAppletHeadlessEpic a = new WorkAppletHeadlessEpic();
     if(args[0]!=null)urlArray.add(args[0]);
