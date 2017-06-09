ee5753d665a818f6a4a76c58b55e3b0e8365f8ec
Colin Kingen <colinwkingen@gmail.com>
2017-06-09 16:32:15 -0700

    DUML in ACCESS : finish parsing date w/ start time, add a couple other columns, tallies and error correction

--- src/main/java/org/ecocean/servlet/importer/AccessImport.java
+++ src/main/java/org/ecocean/servlet/importer/AccessImport.java
@@ -3,6 +3,8 @@ package org.ecocean.servlet.importer;
 import org.ecocean.*;
 import org.ecocean.servlet.*;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.ecocean.identity.*;
 import org.ecocean.media.*;
 
@@ -98,8 +100,8 @@ public class AccessImport extends HttpServlet {
     out.println("********************* Here's the tables : "+tables.toString());
     
     
-    // I'm gonna be super rigid and specific about how tables get processed. This is a complex dataset, and 
-    // I don't want to miss it if something goes subtly wrong. 
+    // I'm gonna be super rigid about how tables get processed. This is a complex dataset, and 
+    // I don't want to miss it if something goes subtly wrong. Exceptions galore should happen if it all isn't perfect.
     myShepherd.beginDBTransaction();
     try {
       out.println("********************* Let's process the DUML Table!");
@@ -133,9 +135,14 @@ public class AccessImport extends HttpServlet {
   private void processDUML(Table table) {
     out.println("DUML Table has "+table.getRowCount()+" Rows!");
     
+    // We're gonna keep a counter of everything. I mean Everything. Deal with it.
+    int errors = 0;
+    
     int locations = 0;
     int dates = 0;
     int projects = 0;
+    int speciesIds = 0;
+    int behaviors = 0;
     
     Row thisRow = null;
     Encounter newEnc = null;
@@ -148,45 +155,52 @@ public class AccessImport extends HttpServlet {
         out.println("!!!!!!!!!!!!!! Could not get next Row in DUML table...");
       }
       // Get the date. 
+      out.println("----------------- ROW : "+i); 
       try {
         String date = null;
-        if (thisRow.containsKey("DATE")) {
+        String startTime = null;
+        if (thisRow.get("DATE") != null) {
+          if (thisRow.get("StartTime") != null) {
+            startTime = thisRow.get("StartTime").toString();
+          }
           date = thisRow.get("DATE").toString();
           out.println("---------------- Date : "+date);    
-          newEnc.setVerbatimEventDate(date);          
-          DateTime dateTime = dateStringToDateTime(date);
+          String verbatimDate = processDateString(date, startTime);
+          
+          DateTime dateTime = dateStringToDateTime(verbatimDate);
+          
+          newEnc.setVerbatimEventDate(dateTime.toString());          
           newEnc.setDateInMilliseconds(dateTime.getMillis());  
           dates += 1;
+          out.println("--------------------------------------------- Stored Date : "+dateTime.toString()+" Stored startTime : "+startTime);
         } 
         // Lets crush that into a DateTime for milli's and stuff.. 
       } catch (Exception e) {
         out.println("!!!!!!!!!!!!!! Could not process a date for row "+i+" in DUML");
         e.printStackTrace();
+        errors +=1;
       }
       
       //get the Location
       try {
         String location = null;
-        if (!thisRow.containsKey("Location")) {
+        if (thisRow.get("Location") != null) {
           location = thisRow.get("Location").toString();          
-        }
-        if (!location.equals(null)) {
-          out.println("---------------- Location : "+location);
           newEnc.setVerbatimLocality(location);    
           locations += 1;
+          out.println("---------------- Location : "+location);
         }
       } catch (Exception e) {
         out.println("!!!!!!!!!!!!!! Could not process a location for row "+i+" in DUML");
         e.printStackTrace();
+        errors +=1;
       }
       
       //get the Project
       try {
         String project = null;
-        if (!thisRow.containsKey("Project")) {
+        if (thisRow.get("Project") != null) {
           project = thisRow.get("Project").toString();          
-        }
-        if (!project.equals(null)) {
           out.println("---------------- Location : "+project);
           newEnc.setSubmitterProject(project);    
           projects += 1;
@@ -194,12 +208,63 @@ public class AccessImport extends HttpServlet {
       } catch (Exception e) {
         out.println("!!!!!!!!!!!!!! Could not process a project for row "+i+" in DUML");
         e.printStackTrace();
+        errors +=1;
       }
+      
+      // Get the Species ID
+      try {
+        String speciesId = null;
+        if (thisRow.get("SPECIES_ID") != null) {
+          speciesId = thisRow.get("SPECIES_ID").toString();          
+          out.println("---------------- Species_ID : "+speciesId);
+          newEnc.setGenus(speciesId);
+          newEnc.setSpecificEpithet(speciesId);
+          speciesIds += 1;
+        }
+      } catch (Exception e) {
+        out.println("!!!!!!!!!!!!!! Could not process a speciesId for row "+i+" in DUML");
+        e.printStackTrace();
+        errors +=1;
+      }
+      
+      // Get the Behavior
+      try {
+        String behavior = null;
+        if (thisRow.get("SPECIES_ID") != null) {
+          behavior = thisRow.get("SPECIES_ID").toString();          
+          out.println("---------------- Behavior : "+behavior);
+          newEnc.setGenus(behavior);    
+          speciesIds += 1;
+        }
+      } catch (Exception e) {
+        out.println("!!!!!!!!!!!!!! Could not process a behavior for row "+i+" in DUML");
+        e.printStackTrace();
+        errors +=1;
+      }
+      
+      // Get the maximum depth.
+      try {
+        String depth = null;
+        if (thisRow.get("Depth") != null) {
+          depth = thisRow.get("Depth").toString();          
+          out.println("---------------- Depth : "+depth);
+          Double depthLong = Double.parseDouble(depth);
+          newEnc.setMaximumDepthInMeters(depthLong);    
+          speciesIds += 1;
+        }
+      } catch (Exception e) {
+        out.println("!!!!!!!!!!!!!! Could not process a MaxDepth for row "+i+" in DUML");
+        e.printStackTrace();
+        errors +=1;
+      }
+      
+      // Let's make a method here that looks at an array of all the columns, removes them if they are processed and tells you if you missed any.
              
     }
     out.println("************** Locations vs rows: "+locations+"/"+table.getRowCount());
     out.println("************** Dates vs rows: "+dates+"/"+table.getRowCount());
     out.println("************** Projects vs rows: "+projects+"/"+table.getRowCount());
+    out.println("******* !!!! TOTALLY CRUSHED IT !!!! *******");
   }
   
   private void processSightings(Table table) {
@@ -210,17 +275,48 @@ public class AccessImport extends HttpServlet {
     out.println("Catalog Table has "+table.getRowCount()+" Rows!");
   }
   
-  private DateTime dateStringToDateTime(String date) {
-    DateFormat fm = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+  private DateTime dateStringToDateTime(String verbatim) {
+    DateFormat fm = new SimpleDateFormat("EEE MMM dd hh:mm a yyyy");
     Date d = null;
     try {
-      d = (Date)fm.parse(date);    
+      d = (Date)fm.parse(verbatim);    
     } catch (ParseException pe) {
       pe.printStackTrace();
+      out.println("Barfed Parsing a Datestring...");
     }
     DateTime dt = new DateTime(d);
+    
     return dt;
   }
+  
+  private String processDateString(String date, String startTime) {
+    String justDate = date.substring(0,11);
+    String years = date.substring(date.length() - 5);
+    String formattedStartTime = formatMilitaryTime(startTime);
+    String finalDateTimeString = justDate + formattedStartTime + years;
+    
+    return finalDateTimeString;
+  }
+  
+  private String formatMilitaryTime(String mt) {
+    
+    // The parsing breaks on military time formatted like "745" instead of "0745"
+    // Stupid timey stuff. Sometimes there are colons, sometimes not. 
+    if (mt.contains(":")) {
+      mt = mt.replace(":", "");
+    }
+    if (mt.length() < 4) {
+      out.println("Short MT!");
+      
+      mt = "0" + mt;
+    }
+    DateTimeFormatter in = DateTimeFormat.forPattern("HHmm"); 
+    DateTimeFormatter out = DateTimeFormat.forPattern("hh:mm a"); 
+    DateTime mtFormatted = in.parseDateTime(mt); 
+    String standard = out.print(mtFormatted.getMillis());
+    
+    return standard;
+  } 
 }
   
   
