af1014156eae8102258b4458042eae4e231c52f3
Colin Kingen <colinwkingen@gmail.com>
2017-07-26 14:36:02 -0700

    when importing images, iterate through metadata excel sheets for linking

--- src/main/java/org/ecocean/servlet/importer/ImportReadImages.java
+++ src/main/java/org/ecocean/servlet/importer/ImportReadImages.java
@@ -10,7 +10,8 @@ import org.ecocean.*;
 import org.ecocean.servlet.*;
 import org.joda.time.DateTime;
 import org.ecocean.media.*;
-
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 //import org.apache.poi.hssf.usermodel.*;
 //import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -30,6 +31,8 @@ public class ImportReadImages extends HttpServlet {
   private static final long serialVersionUID = 1L;
   private static PrintWriter out;
   private static String context; 
+  private int failedAssets = 0;
+  private int assetsCreated = 0;
   
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
@@ -44,7 +47,7 @@ public class ImportReadImages extends HttpServlet {
     context = ServletUtilities.getContext(request);
     out.println("Preparing to import image files.");
     Shepherd myShepherd = new Shepherd(context);
-    myShepherd.setAction("ImpeortReadImages.class");
+    myShepherd.setAction("ImportReadImages.class");
     myShepherd.beginDBTransaction();
     if (!CommonConfiguration.isWildbookInitialized(myShepherd)) {
       out.println("WARNING: Wildbook not initialized. Starting Wildbook");    
@@ -56,17 +59,16 @@ public class ImportReadImages extends HttpServlet {
     File rootFile = new File(imageDir);
     out.println(rootFile.getAbsolutePath());
     if (rootFile.exists()) {
-      out.println("Exists!");
+      out.println("Directory Exists Hooray!");
     } else {
-      out.println("Doesn't Exist.");
+      out.println("The Specified Directory Doesn't Exist.");
     }
+    getFiles(rootFile, myShepherd); 
     
-    getFiles(rootFile, myShepherd);
+    out.println("Created "+assetsCreated+" new MediaAssets.");
+    out.println(failedAssets+" assets failed to be created.");
     
-    
-      
     myShepherd.closeDBTransaction();
-      
   }
   
   public void getFiles(File path, Shepherd myShepherd) {
@@ -78,9 +80,18 @@ public class ImportReadImages extends HttpServlet {
           getFiles(new File(path, subDirs[i]), myShepherd);
         }
       }
-      if (path.isFile()) {
+      if (path.isFile()&&!path.getName().endsWith("xlsx")) {
         out.println("Found file: "+path.getName());
-        processImage(path, myShepherd);
+        //boolean success = processImage(path, myShepherd);
+        boolean success = true;
+        if (success) {
+          assetsCreated++;
+        } else {
+          failedAssets++;
+        }
+      } 
+      if (path.isFile()&&path.getName().endsWith("xlsx")) {
+        collectExcelData(path, myShepherd);
       }
       if (path.isDirectory()) {
         out.println("Found Directory: "+path.getAbsolutePath());
@@ -89,42 +100,85 @@ public class ImportReadImages extends HttpServlet {
       e.printStackTrace();
       out.println("Failed to traverse Image and excel files at path "+path.getAbsolutePath()); 
     }
+    out.println("Found Directory: "+path.getAbsolutePath());
   }
   
-  public void processImage(File image, Shepherd myShepherd) {
-    int totalAssets = 0;
+  public boolean processImage(File image, Shepherd myShepherd) {
     AssetStore assetStore = AssetStore.getDefault(myShepherd);
     JSONObject params = new JSONObject();
     MediaAsset ma = null;
     File photo = null;
+    //out.println("Image Path? : /"+FilenameUtils.getPath(image.getAbsolutePath()));
+    //out.println("Image Name? : "+image.getName());
     try {
-      photo = new File(image.getPath(),image.getName());
+      photo = new File("/"+FilenameUtils.getPath(image.getAbsolutePath()),image.getName());
+      params = assetStore.createParameters(photo);
       ma = new MediaAsset(assetStore, params);
+      ma.addDerivationMethod("Bass Importer", System.currentTimeMillis());
+      ma.addLabel("_original");
       ma.copyIn(photo);
     } catch (Exception e) {
       e.printStackTrace();
       out.println("!!!! Error Trying to Create Media Asset!!!!");
+      return false;
     }
     if (ma!=null) {
       try {
         myShepherd.beginDBTransaction();
         myShepherd.getPM().makePersistent(ma);
         myShepherd.commitDBTransaction();
+        ma.updateMetadata();
         ma.updateStandardChildren(myShepherd);
-        totalAssets++;
       } catch (Exception e) {
         myShepherd.rollbackDBTransaction();
         e.printStackTrace();
         out.println("!!!! Could not Persist Media Asset !!!!");
-      }      
-    }
-    out.println("Created "+totalAssets+" new MediaAssets.");
+        return false;
+      }
+    }      
+    out.println("Created a new MediaAsset. Filename : "+assetStore.getFilename(ma));
+    return true;
   }
   
-  public void proessExcel(File file) {
+  public void collectExcelData(File file, Shepherd myShepherd) throws IOException { 
+    out.println("\nHey! It's an excel file! Nom Nom.");
+    // We are going to make a huge Map of all the metadata we need from Excel, and store 
+    // it to process later after all images are in. The key will be the image filename.
+
+    HashMap<String,HashMap<String,String>> data = new HashMap<String,HashMap<String,String>>();
+    FileInputStream fs = new FileInputStream(file);
+    XSSFWorkbook wb = new XSSFWorkbook(fs);
+    XSSFSheet sheet = wb.getSheetAt(0);
+    XSSFRow row = null;
     
+    int rows = sheet.getPhysicalNumberOfRows();
+    int cols = 0;
+    
+    HashMap<String,String> rowData = null;
+
+    // Triple for loops? Has the world gone mad?
+    for (int i=0;i<rows;i++) {
+      row = sheet.getRow(i);
+      for (int k=0;k<2;k++) {
+        sheet = wb.getSheetAt(k);
+        cols = sheet.getRow(k).getPhysicalNumberOfCells();
+        for (int j=0;j<cols;j++) {
+          rowData = new HashMap<String,String>(19);
+          XSSFCell cell = row.getCell(j);
+          String cellKey = cell.getSheet().getRow(0).getCell(j).getRichStringCellValue().toString();
+          String cellValue = cell.getStringCellValue();
+          if (cellValue!=null&&cellValue!="") {
+            rowData.put(cellKey, cellValue);
+            out.println("Adding Key : "+cellKey+" Value : "+cellValue);
+          }
+        }
+      }
+      System.out.println(rowData.toString());
+      data.put(rowData.get("image_file"), rowData);
+    }
+    wb.close();
   }
-    
+
 }
 
 
