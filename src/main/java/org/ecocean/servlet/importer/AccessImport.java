9c6deeb3fca98fce62649d003e1b2d14d9394aa2
Colin Kingen <colinwkingen@gmail.com>
2017-10-03 16:42:08 -0700

    add more info to outer survey.jsp, fix embedded map auto center

--- src/main/java/org/ecocean/servlet/importer/AccessImport.java
+++ src/main/java/org/ecocean/servlet/importer/AccessImport.java
@@ -121,7 +121,7 @@ public class AccessImport extends HttpServlet {
     
     
     // These switches allow you to work on different tables without doing the whole import a bunch of times.
-    boolean dumlTableSwitch = false;
+    boolean dumlTableSwitch = true;
     if (dumlTableSwitch) {    
       try {
         out.println("********************* Let's process the DUML Table!\n");
@@ -135,7 +135,7 @@ public class AccessImport extends HttpServlet {
       }
     }  
     
-    boolean simpleLocationsDUML = false;
+    boolean simpleLocationsDUML = true;
     if (simpleLocationsDUML) {
       try {
         out.println("********************* Building a HashMap of simple location names for EFFORT/DUML matching...");
@@ -148,7 +148,7 @@ public class AccessImport extends HttpServlet {
       }
     }
     
-    boolean sightingsTableSwitch = false;
+    boolean sightingsTableSwitch = true;
     if (sightingsTableSwitch) {
       try {
         out.println("********************* Let's process the SIGHTINGS Table!\n");
@@ -172,7 +172,7 @@ public class AccessImport extends HttpServlet {
       }      
     }
     
-    boolean biopsyTableSwitch = false;
+    boolean biopsyTableSwitch = true;
     if (biopsyTableSwitch) {
       try {
         out.println("********************* Let's process the BiopsySamples Table!\n");
